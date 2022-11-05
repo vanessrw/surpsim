5ffc4eb4795382b4dbe039758ca4ee25e3f3561b
vanessrw <64979714+vanessrw@users.noreply.github.com>
2022-11-06 00:54:59 +0700

    update

---
+++ components/organism/EventGz/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import SubHeader from 'components/atoms/SubHeader';
+
+export default function EventHs() {
+
+  return (
+    <div className="eventgz">
+      <div className="vidgz">
+        <div className="vid1gz">
+          <Link href="https://youtu.be/4M_4y75lfO8" passHref>
+            <Image
+              data-aos="fade-up"
+              className="thumbnail"
+              src="/foto/gz1.jpg"
+              alt="vid thumbnail"
+              width={300}
+              height={300}
+            ></Image>
+          </Link>
+        </div>
+        <div className="event-content">
+          <div className="content">
+            <SubHeader title="VANES" subtitle="" left fade="fade-up" />
+            <p data-aos="fade-up">{"wishnya dari vid aja ya mwehehe"}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
